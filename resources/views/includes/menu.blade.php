fb09ec4b004ce73e16bd3658671cf6f2c1553f96
Matheus <matheus.hack@ig.com.br>
2018-09-02 13:46:02 -0300

    template modelo

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -81,6 +81,42 @@
                     </span>
                 </a>
             </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true"  m-menu-submenu-toggle="hover">
+                <a  href="javascript:;" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-map-location"></i>
+                    <span class="m-menu__link-text">Sistema</span>
+                    <i class="m-menu__ver-arrow la la-angle-right"></i>
+                </a>
+                <div class="m-menu__submenu ">
+                    <span class="m-menu__arrow"></span>
+                    <ul class="m-menu__subnav">
+                        <li class="m-menu__item " aria-haspopup="true" >
+                            <a  href="components/base/state.html" class="m-menu__link ">
+                                <i class="m-menu__link-icon flaticon-cogwheel"></i>
+                                <span class="m-menu__link-text">Usuários</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true" >
+                            <a  href="components/base/state.html" class="m-menu__link ">
+                                <i class="m-menu__link-icon flaticon-profile-1"></i>
+                                <span class="m-menu__link-text">Perfis</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true" >
+                            <a  href="components/base/state.html" class="m-menu__link ">
+                                <i class="m-menu__link-icon flaticon-squares-4"></i>
+                                <span class="m-menu__link-text">Permissões</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true" >
+                            <a  href="components/base/state.html" class="m-menu__link ">
+                                <i class="m-menu__link-icon flaticon-like"></i>
+                                <span class="m-menu__link-text">Parâmetros</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
         </ul>
     </div>
 </div>
